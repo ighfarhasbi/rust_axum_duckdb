2fa381f7bbd5c44875365375cbda2fcbe1994d6c
ighfarhasbi <ighfarhasbiash@gmail.com>
2024-06-12 17:26:32 +0700

    add update, delete, file parquet encript

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,11 +1,12 @@
 use axum::{
-    extract::{Extension, Json},
+    extract::{Extension, Json, Path as JsonPath},
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+// use chrono::NaiveDate;
 use duckdb::{params, Connection};
 use serde_json::json;
-use std::{fs, path::Path, sync::{Arc, Mutex}};
+use std::{fs, path::Path, sync::{Arc, Mutex, MutexGuard}};
 use crate::{models::Mahasiswa, response_model::ResponseModel};
 
 pub async fn get_mahasiswa(
@@ -49,45 +50,62 @@ pub async fn add_mahasiswa(
     stmt.execute(params![item.id, item.nama, item.tgl_lahir])
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
-    let _exp = conn.execute("COPY data_mahasiswa TO 'coba_mahasiswa_exp.parquet' (FORMAT PARQUET);", [])
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-
-    // Persiapan path setelah export sukses
-    let src_path = Path::new("coba_mahasiswa_exp.parquet");
-    let dest_dir = Path::new("src/parquet");
-    let dest_path = dest_dir.join("coba_mahasiswa_exp.parquet");
-
-    // memastikan directory ada
-    if !dest_dir.exists() {
-        fs::create_dir_all(dest_dir).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-    }
-
-    // memindahkan file hasil export
-    fs::rename(src_path, dest_path).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-
+    to_parquet(&conn); // buat file parquet
 
     Ok((StatusCode::OK, Json(json!({"status": "success"}))).into_response())
 }
 
-pub async fn export_mahasiswa (
-    Extension(conn): Extension<Arc<Mutex<Connection>>>,
-) -> Result<Response, (StatusCode, String)> {
-    let conn = conn.lock().map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to lock connection".to_string()))?;
-    let _exp = conn.execute("COPY data_mahasiswa TO 'coba_mahasiswa_exp.parquet' (FORMAT PARQUET);", [])
+pub async fn update_mahasiswa (conn: Extension<Arc<Mutex<Connection>>>, id: JsonPath<i32>, Json(item): Json<Mahasiswa>) -> Result<Response, (StatusCode, String)> {
+    let conn = conn.lock().map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    let mut stmt = conn.prepare("UPDATE data_mahasiswa SET nama = ?, tgl_lahir = ? WHERE id = ?")
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    stmt.execute(params![item.nama, item.tgl_lahir, id.to_string()])
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
-    // Persiapan path setelah export sukses
-    let src_path = Path::new("coba_mahasiswa_exp.parquet");
-    let dest_dir = Path::new("src/parquet");
-    let dest_path = dest_dir.join("coba_mahasiswa_exp.parquet");
+    to_parquet(&conn); // update file parquet
 
-    // memastikan directory ada
-    if !dest_dir.exists() {
-        fs::create_dir_all(dest_dir).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-    }
+    Ok((StatusCode::OK, Json(json!({"status": "sukses" }))).into_response())
 
-    // memindahkan file hasil export
-    fs::rename(src_path, dest_path).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+}
+
+pub async fn delete_mahasiswa (conn: Extension<Arc<Mutex<Connection>>>, id: JsonPath<i32>) -> Result<Response, (StatusCode, String)> {
+    let conn = conn.lock().map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to lock connection".to_string()))?;
+    let mut stmt = conn.prepare("DELETE FROM data_mahasiswa WHERE id =?")
+       .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    stmt.execute(params![id.to_string()])
+       .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    to_parquet(&conn); // update file parquet
+
+    Ok((StatusCode::OK, Json(json!({"status": "sukses" }))).into_response())
+}
+
+pub async fn export_mahasiswa (
+    Extension(conn): Extension<Arc<Mutex<Connection>>>,
+    nama_file: JsonPath<String>
+) -> Result<Response, (StatusCode, String)> {
+    let conn = conn.lock().map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to lock connection".to_string()))?;
+     // Prepare the SQL statement
+     let sql = format!("COPY data_mahasiswa TO '{}' (FORMAT PARQUET);", nama_file.to_string());
+     let mut stmt = conn.prepare(&sql).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+     
+     // Execute the statement
+     stmt.execute([]).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+ 
+    
+
+    // // Persiapan path setelah export sukses
+    // let src_path = Path::new("coba_mahasiswa_exp.parquet");
+    // let dest_dir = Path::new("src/parquet");
+    // let dest_path = dest_dir.join("coba_mahasiswa_exp.parquet");
+
+    // // memastikan directory ada
+    // if !dest_dir.exists() {
+    //     fs::create_dir_all(dest_dir).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    // }
+
+    // // memindahkan file hasil export
+    // fs::rename(src_path, dest_path).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
 
     Ok((StatusCode::OK, Json(json!({"status": "sukses" }))).into_response())
@@ -98,4 +116,27 @@ fn duckdb_date_to_string(date: i32) -> String {
     let unix_epoch = chrono::NaiveDate::from_ymd(1970, 1, 1);
     let date = unix_epoch + chrono::Duration::days(epoch_days);
     date.to_string()
+}
+
+fn to_parquet(conn: &MutexGuard<Connection>) {
+    let _exp = conn.execute("COPY data_mahasiswa TO 'coba_mahasiswa_exp.parquet' (FORMAT PARQUET);", [])
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()));
+
+    let _set_key = conn.execute("PRAGMA add_parquet_key('key256', '01234567891123450123456789112345');", [])
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()));
+    let _set_key = conn.execute("COPY coba_mahasiswa_exp.parquet TO 'coba_mahasiswa_exp_encrpt256.parquet' (ENCRYPTION_CONFIG {footer_key: 'key256'});", [])
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()));
+
+    // Persiapan path setelah export sukses
+    let src_path = Path::new("coba_mahasiswa_exp_encrpt256.parquet"); 
+    let dest_dir = Path::new("src/parquet");
+    let dest_path = dest_dir.join("coba_mahasiswa_exp_encrpt256.parquet");
+
+    // jika folder src/parquet tidak ada, maka akan dibuat dulu
+    if !dest_dir.exists() {
+        let _add_dir = fs::create_dir_all(dest_dir).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()));
+    }
+
+    // jika folder src/parquet ada, maka pindahkan file .parquet
+    let _rename = fs::rename(src_path, dest_path).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()));
 }
--- src/router.rs
+++ src/router.rs
@@ -1,18 +1,18 @@
 // src/router.rs
 
 use axum::{
-    routing::{get, post},
-    Router,
-    Extension,
+    routing::{delete, get, post, put}, Extension, Router
 };
 use std::sync::{Arc, Mutex};
-use crate::handlers::{get_mahasiswa, add_mahasiswa, export_mahasiswa};
+use crate::handlers::{get_mahasiswa, add_mahasiswa, export_mahasiswa, update_mahasiswa, delete_mahasiswa};
 use duckdb::Connection;
 
 pub fn create_router(conn: Arc<Mutex<Connection>>) -> Router {
     Router::new()
         .route("/mahasiswa", get(get_mahasiswa))
         .route("/mahasiswa", post(add_mahasiswa))
-        .route("/export-mahasiswa", get(export_mahasiswa))
+        .route("/mahasiswa/:id", put(update_mahasiswa))
+        .route("/mahasiswa/:id", delete(delete_mahasiswa))
+        .route("/export-mahasiswa/:nama_file", get(export_mahasiswa))
         .layer(Extension(conn))
 }
